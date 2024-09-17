2659788b69736411f8f1f1ba67fa9f996e497dd6
Iñaki <irosello@outlook.com.ar>
2024-09-16 21:02:34 -0300

    [ADD]Adding all files

---
+++ main.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import pickle
+from fastapi import FastAPI
+
+from fastapi.encoders import jsonable_encoder
+import uvicorn
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+COLUMNS_PATH = "model/categorias_ohe.pkl"
+with open(COLUMNS_PATH, 'rb') as handle:
+    ohe_tr = pickle.load(handle)
+
+MODEL_PATH = "model/rf.pkl"
+model = pickle.load(open(MODEL_PATH, 'rb'))
+
+class Answer(BaseModel):
+    ph: float
+    Hardness: float
+    Solids: float
+    Chloramines: float
+    Sulfate: float
+    Conductivity: float
+    Organic_carbon: float
+    Trihalomethanes: float
+    Turbidity: float
+
+@app.get("/")
+async def root():
+    return {"message":"Model Deployment Bootcamp"}
+
+@app.post("/prediction")
+def predict_water_potability(answer:Answer):
+    answer_dict = jsonable_encoder(answer)
+
+    for key, value in answer_dict.items():
+        answer_dict[key] = [value]
+
+    single_instance = pd.DataFrame.from_dict(answer_dict)
+
+    single_instance_ohe = pd.get_dummies(single_instance).reindex(columns = ohe_tr).fillna(0)
+
+    prediction = model.predict(single_instance_ohe)
+    
+    score = "no" if int(prediction[0]) == 0 else "yes"
+ 
+    response = {"Potable Water: ": score}
+
+    return response
+
+
+if __name__ == "__main__":
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
