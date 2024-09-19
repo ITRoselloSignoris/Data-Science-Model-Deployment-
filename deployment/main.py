3d768d950a2fc6b4c765aca4a10335a0cf5b2f25
Iñaki <irosello@outlook.com.ar>
2024-09-19 00:38:55 -0300

    [UPDATE]Changes made to save bins and use them in the API

--- deployment/main.py
+++ deployment/main.py
@@ -13,6 +13,18 @@ COLUMNS_PATH = "../src/categorias_ohe.pkl"
 with open(COLUMNS_PATH, 'rb') as handle:
     ohe_tr = pickle.load(handle)
 
+BINS1_PATH = "../src/saved_bins/ph_bins.pkl"
+with open(BINS1_PATH, 'rb') as handle:
+    ph_bins = pickle.load(handle)
+
+BINS2_PATH = "../src/saved_bins/Trihalomethanes_bins.pkl"
+with open(BINS2_PATH, 'rb') as handle:
+    Trihalomethanes_bins = pickle.load(handle)
+
+BINS3_PATH = "../src/saved_bins/Sulfate_bins.pkl"
+with open(BINS3_PATH, 'rb') as handle:
+    Sulfate_bins = pickle.load(handle)
+
 MODEL_PATH = "../src/model/rf.pkl"
 model = pickle.load(open(MODEL_PATH, 'rb'))
 
@@ -38,8 +50,14 @@ def predict_water_potability(answer:Answer):
     for key, value in answer_dict.items():
         answer_dict[key] = [value]
 
-    single_instance = pd.DataFrame.from_dict(answer_dict)
+    single_instance = pd.DataFrame.from_dict(answer_dict) 
 
+    single_instance["ph_discretized"] = pd.cut(single_instance["ph"],bins = ph_bins,include_lowest = True)
+    single_instance["Trihalomethanes_ph_discretized"] = pd.cut(single_instance["Trihalomethanes"],bins = Trihalomethanes_bins,include_lowest = True)
+    single_instance["Sulfate_discretized"] = pd.cut(single_instance["Sulfate"],bins = Sulfate_bins,include_lowest = True)
+    
+    single_instance = single_instance.drop(columns=["ph","Trihalomethanes","Sulfate"],axis=1)
+    
     single_instance_ohe = pd.get_dummies(single_instance).reindex(columns = ohe_tr).fillna(0)
 
     prediction = model.predict(single_instance_ohe)
